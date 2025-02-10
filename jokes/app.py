d91bea227ca4e56a25bd1559138c71defed03a79
kainatfatima1605 <kainatrafiq1625@gmail.com>
2025-02-10 10:43:33 -0800

    python user

--- jokes/app.py
+++ jokes/app.py
@@ -1,3 +1,11 @@
-import  pyjokes
+
+# -------------- pyjokes-------------
+""""import  pyjokes
 joke = pyjokes.get_joke()
-print(joke)
+print(joke)"""
+
+# -----------python text  to specch---------------------
+import pyttsx3
+engine = pyttsx3.init()
+engine.say(  "Dreams begin with belief."  )
+engine.runAndWait()
