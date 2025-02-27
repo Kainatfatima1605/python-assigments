b74cd327b1860f72c9c501f5dc833f99eab27ff0
kainatfatima1605 <kainatrafiq1625@gmail.com>
2025-02-27 14:21:25 -0800

    fromat  method

---
+++ practice/app.py
@@ -0,0 +1,9 @@
+#------------------ format pratice ------------------------------
+
+name = input("enter your name" )
+age= input("enter your age" )
+height= input("enter your height" )
+qualification =input("enter your education" )
+
+ans="your name is {}. your age is{} your height is {}.your qualifation ".format(name,age,height,qualification)
+print(ans)
